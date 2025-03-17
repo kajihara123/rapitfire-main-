298b93fe3a46abf4371cdd836be1ca8ae8bfd2f2
hatakeyama <00116202111@st.emtech-academy.com>
2025-03-17 10:49:38 +0900

    畠山作業

---
+++ 7segLcd.c
@@ -0,0 +1,93 @@
+#include	"iodefine.h"
+#include	<stdio.h>
+#include	"7segLcd.h"
+
+
+static char g_hexNum[] = {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+static char	g_digit[4];
+static char g_errorHex[3] = {0x79, 0x50, 0x50};
+
+
+
+
+void init7SEG(void)
+{
+	PORTE.PODR.BYTE   = 0x00;	/* PE0-PE7 OFF                     */
+	PORTE.PDR.BYTE    = 0xFF;	/* PE0-PE7 出力ポート設定          */
+
+	
+//	PORTD.PODR.BYTE &= 0x07;	/* PD3-7   OFF                     */
+//	PORTD.PDR.BYTE  |= 0xF8;	/* PD3-7   出力ポート設定          */
+	PORTD.PODR.BYTE = 0x00;
+	PORTD.PDR.BYTE 	= 0xFB;
+	
+}
+
+void displayHits(void)
+{
+	static int pos = 0;
+	
+	processHits();
+	
+	preparat7seg();
+
+	if (pos < 4) {
+		PORTD.PODR.BYTE |= 0x80 >> pos;
+	}
+	
+	PORTE.PODR.BYTE = g_hexNum[g_digit[pos]];
+		
+	pos++;
+	if (pos > 3) {
+		pos = 0;
+	}
+}
+
+
+void processHits(void)
+{
+	int hits;
+	
+	if(g_playerNum == 1){
+		hits = g_myData;
+	}else if(g_playerNum == 2){
+		hits = g_pairData;
+	}
+		
+	g_digit[0] = hits / 1000;	
+	hits %= 1000;
+	g_digit[1] = hits / 100;	
+	hits %= 100;
+	g_digit[2] = hits / 10;	
+	hits %= 10;
+	g_digit[3] = hits;		
+}
+
+void displayError(void)
+{
+	static int pos = 0;
+	
+	preparat7seg();
+	
+	if (pos < 4) {
+		PORTD.PODR.BYTE |= 0x80 >> pos;
+	}
+	
+	if(pos < 3){
+	PORTE.PODR.BYTE = g_errorHex[pos];
+	}
+	
+	pos++;
+	if (pos > 3) {
+		pos = 0;
+	}
+}
+
+void preparat7seg(void)
+{
+	PORTE.PODR.BYTE   = 0x00;
+	PORTJ.PODR.BIT.B3 = 0;
+	
+	PORTD.PODR.BYTE = 0x00; 
+		
+}	
---
+++ 7segLcd.h
@@ -0,0 +1,10 @@
+#ifndef D7SEG_LCD_H
+#define D7SEG_LCD_H
+
+void init7SEG(void);
+void displayHits(void);
+void processHits(void);
+void displayError(void);
+void preparat7seg(void);
+
+#endif
