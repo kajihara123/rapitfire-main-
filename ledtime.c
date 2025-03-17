a409546fbbc8e293a320b8bf692beca0a2c68bda
ohara <00116202102@st.emtech-academy.com>
2025-03-17 15:02:39 +0900

    変更

--- ledtime.c
+++ ledtime.c
@@ -1,26 +1,29 @@
 #include "iodefine.h"
 
-void ledTime(void)
+int ledTime(void)
 {
     static int cnt = 0;               /* カウントダウンと時間計測用変数 */
     static int value = 0;      // カウントダウン用の変数
-    static int ledShift = 0x07;  // LEDの初期状態（3つのLEDが点灯）
+    static int ledShift = 0x0F;  // LEDの初期状態（3つのLEDが点灯）
     PORTJ.PDR.BIT.B3 = 1; 
     PORTJ.PODR.BIT.B3 = 1;
     
     PORTE.PDR.BYTE = 0xFF;    // ポートEを出力に設定
     PORTE.PODR.BYTE = 0x00;   // 初期状態: LEDは全て消灯
+    PORTE.PODR.BYTE = ledShift;
+
+    while(1){
 
     // ゲーム開始前のカウントダウン（g_gameMode == 1）
     if (g_gameMode == 1) {
-        // cntが31500回進んだらLEDをシフト
-        if (cnt % 31500 == 0) {
+        // cntが10000回進んだらLEDをシフト
+        if (cnt % 10000 == 0) {
             // 3, 2, 1 カウントダウン
-            PORTE.PODR.BYTE = ledShift;  // LEDの状態を変更
-            ledShift >>= 1;  // LEDを1ビットシフト
+//            PORTE.PODR.BYTE = ledShift;  // LEDの状態を変更
+            PORTE.PODR.BYTE = PORTE.PIDR.BYTE>>1;  // LEDを1ビットシフト
 
             // カウントダウンが終わったら次に進む
-            if (ledShift == 0x00) {  
+            if (PORTE.PIDR.BYTE == 0x00) {  
                 g_gameMode = 2;  // ゲームモードを2に変更
                 PORTE.PODR.BYTE = 0xFF;  // 全LED点灯
             }
@@ -30,21 +33,20 @@ void ledTime(void)
     // ゲーム中（g_gameMode == 2）
     if (g_gameMode == 2) {
         // 約2.5秒ごとにLEDをシフト
-        if (cnt % 78750 == 0) {
-            PORTE.PODR.BYTE = ledShift;  // LEDの状態を変更
-            ledShift >>= 1;  // LEDを1ビットシフト
+        if (cnt % 10000 == 0) {
+            PORTE.PODR.BYTE = PORTE.PIDR.BYTE>>1;  // LEDを1ビットシフト
 
             // LEDのシフトが終わったら、次に進む
-            if (ledShift == 0x00) {
+            if (PORTE.PIDR.BYTE == 0x00) {
                 g_gameMode = 3;  // ゲームモードを3に変更（終了）
             }
-
-            cnt = 0;  // カウントリセット
         }
     }
 
     cnt++;  // カウントアップ
+    }
 }
+
 	/*
 
 	cnt++;
