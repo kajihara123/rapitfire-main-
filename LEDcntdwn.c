71ba58769760d37d5db2149d8f95b3773574d0d9
ohara <00116202102@st.emtech-academy.com>
2025-03-17 10:54:30 +0900

    LED動きはしたので確認お願いします。

--- LEDcntdwn.c
+++ LEDcntdwn.c
@@ -1,8 +1,8 @@
-include"iodefine.h"
+#include"iodefine.h"
 
-int main(void)
+void main(void)
 {
-    int cnt = 3;                    /*カウントダウンと時間計測用変数*/
+    int cnt =0;                    /*カウントダウンと時間計測用変数*/
 	char y = 0x07;                  /*LED用変数*/
 	int weight = 0;                 /*タイマカウンタ用ウェイト*/
 	PORTJ.PDR.BIT.B3 = 1; 
@@ -11,10 +11,15 @@ int main(void)
 	PORTE.PDR.BYTE = 0xFF;
 	PORTE.PODR.BYTE = 0x00;
 
-    PORTE.PODR.BYTE = y;
-    for(i = 3; i > 0; i++){
-        for(weight = 0; weight < 10000; weight++){}
-        PORTE.PODR.BYTE = PORTE.PIDR.BYTE>>1;
+    	PORTE.PODR.BYTE = y;
+    	for(cnt = 0; cnt < 3; cnt++){
+        	for(weight = 0; weight < 31500; weight++){}
+        	PORTE.PODR.BYTE = PORTE.PIDR.BYTE>>1;
+   	}	
 
-    }
+    	for (cnt = 0, PORTE.PODR.BYTE = 0xFF; cnt < 8; cnt++)
+    	{
+        	for(weight = 0; weight < 78750; weight++){}
+        	PORTE.PODR.BYTE = PORTE.PIDR.BYTE>>1;
+	}
 }
