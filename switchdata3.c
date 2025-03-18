cb1589c4715f624047293f06db754e8e58d5020d
kajihara <00116202110@st.emtech-academy.com>
2025-03-18 10:10:00 +0900

    data変更

---
+++ switchdata3.c
@@ -0,0 +1,16 @@
+#include	"iodefine.h"
+#include    <stdio.h>
+#include    "switchdata3.h"
+
+
+void sw3Clt(void)
+{
+	if (PORT0.PIDR.BIT.B5 == 0){
+		if (g_gameMode == 2){
+			g_myData = g_myData + 1;			
+		}	
+		if (g_gameMode == 3){
+			g_playerNum = 1;	/*自分のプレーヤー番号の選択*/
+		}		
+	}
+}
--- switchdate4.h
+++
@@ -1,6 +0,0 @@
-#ifndef switchdate4_H
-#define switchdate4_H
-
-void sw4Clt(void);
-
-#endif
