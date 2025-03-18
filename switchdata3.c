6aaaabc44b484c05622013ccc45a4f61f44acfd7
TOKO <00116202108@st.emtech-academy.com>
2025-03-18 10:14:10 +0900

    changee

--- switchdata3.c
+++ switchdata3.c
@@ -5,17 +5,6 @@
 
 void sw3Clt(void)
 {
-<<<<<<< HEAD
-		if (PORT0.PIDR.BIT.B5 == 0){
-			/*ゲームステータスを0にする*/
-			if (g_gameMode == 2){
-				g_myDate = g_myDate + 1;		
-			/*ゲームモードを2にする*/	
-			}	
-			if (g_gameMode == 3){
-				g_playerNum = 1;	/*自分のプレーヤー番号の選択*/
-			}		
-=======
 	if (PORT0.PIDR.BIT.B5 == 0){
 		if (g_gameMode == 2){
 			g_myData = g_myData + 1;			
@@ -23,6 +12,5 @@ void sw3Clt(void)
 		if (g_gameMode == 3){
 			g_playerNum = 1;	/*自分のプレーヤー番号の選択*/
 		}		
->>>>>>> 05d3ad1080ec3a9fda1f021a420c11f5754d460f
 	}
 }
