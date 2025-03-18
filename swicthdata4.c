4c978404a9f153a6e29dacd21715bdba6a46a874
kajihara <00116202110@st.emtech-academy.com>
2025-03-18 10:47:51 +0900

    完成品

--- swicthdata4.c
+++ swicthdata4.c
@@ -1,17 +1,13 @@
 #include "iodefine.h"
 #include <stdio.h>
 #include "switchdata4.h"
-
+#include "renda.h"
 
 void sw4Clt(void){
 
     if (PORT0.PIDR.BIT.B7 == 0){
 
 		if (g_gameMode == 3){				/*ゲームステータスが３の時に以下の処理を行う*/
-
-		if (g_pairData == -1){
-		 	g_gameMode = -1; 				/*！ステータスを異常状態にする*/
-		}
 			g_playerNum = 2;
 		}
 	}
