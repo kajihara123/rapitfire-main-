4daff0cc1fed3670b4d71b25405932c5639cc820
kajihara <00116202110@st.emtech-academy.com>
2025-03-18 10:19:18 +0900

    2

--- swicthdata4.c
+++ swicthdata4.c
@@ -4,15 +4,14 @@
 
 
 void sw4Clt(void){
-	
-    if (PORT0.PIDR.BIT.B7 == 0)
-	{
-		if (g_gameMode == 3)				/*ゲームステータスが３の時に以下の処理を行う*/
-		{
-		 if (g_pairData == -1)
-		 {
+
+    if (PORT0.PIDR.BIT.B7 == 0){
+
+		if (g_gameMode == 3){				/*ゲームステータスが３の時に以下の処理を行う*/
+
+		if (g_pairData == -1){
 		 	g_gameMode = -1; 				/*！ステータスを異常状態にする*/
-		 }
+		}
 			g_playerNum = 2;
 		}
 	}
