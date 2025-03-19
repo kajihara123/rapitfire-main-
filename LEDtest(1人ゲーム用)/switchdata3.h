1b2fee374ac5c27f9f9e2f926f335db092968cce
kajihara <00116202110@st.emtech-academy.com>
2025-03-19 14:21:22 +0900

    チャタリング対策

--- LEDtest(1人ゲーム用)/switchdata3.h
+++ LEDtest(1人ゲーム用)/switchdata3.h
@@ -1,7 +1,14 @@
 #ifndef swicthdata3_H
 #define switchdata3_H
 
+/***** definition *****/
+#define	SW3	0x20
+#define	SW4	0x80
+#define	SW34	(SW3 | SW4)
+
 void sw3Clt(void);
 void initSWs(void);
+int getSW(int bit);
+int getSWs(void);
 
 #endif
